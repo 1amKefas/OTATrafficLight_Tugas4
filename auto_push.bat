82b73e232994a4722f574382dfc92da9f975a210
kefas hutabarat <kefaskefas10@gmail.com>
2026-04-27 22:47:20 +0700

    Auto-update firmware bin: 27/04/2026 22:47:20,79

--- auto_push.bat
+++ auto_push.bat
@@ -4,10 +4,14 @@ echo   AUTO UPDATE FIRMWARE ESP32 KE GITHUB
 echo ==========================================
 echo.
 
-:: Pindah ke direktori tempat script ini berada (folder repo lu)
+:: Pindah ke direktori tempat script ini berada
 cd /d %~dp0
 
-:: Tambahin semua file yang berubah (termasuk .bin lu yang baru)
+:: Copy file .bin dari dalam folder build ke folder root repo lu
+:: (Pakai /Y supaya otomatis nge-replace file .bin lama tanpa nanya)
+copy /Y "build\esp32.esp32.esp32\OTATrafficLight_Tugas4.ino.bin" "OTATrafficLight_Tugas4.ino.bin"
+
+:: Tambahin semua file ke Git (folder build bakal dicuekin berkat .gitignore)
 git add .
 
 :: Bikin commit otomatis pakai timestamp waktu hari ini
